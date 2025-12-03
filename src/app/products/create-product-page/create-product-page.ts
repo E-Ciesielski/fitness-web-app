97b351da0f1dfabd082bbdc4aa3272a422cbc0fa
Eryk C <eryk9903@gmail.com>
2025-12-03 13:08:11 +0100

    add bootstrap and make product form

--- src/app/products/create-product-page/create-product-page.ts
+++ src/app/products/create-product-page/create-product-page.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-product-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-product-page.html',
   styleUrl: './create-product-page.css',
 })
 export class CreateProductPage {
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(200)]),
+    unit: new FormControl('', [Validators.required]),
+    unitAmount: new FormControl('', [
+      Validators.required,
+      Validators.max(100_000),
+      Validators.min(0),
+    ]),
+    caloriesPerUnit: new FormControl('', [
+      Validators.required,
+      Validators.max(100_000),
+      Validators.min(0),
+    ]),
+    quantity: new FormControl('', [
+      Validators.required,
+      Validators.max(100_000),
+      Validators.min(0),
+    ]),
+  });
 
+  get name() {
+    return this.form.get('name');
+  }
+
+  get unit() {
+    return this.form.get('unit');
+  }
+
+  get unitAmount() {
+    return this.form.get('unitAmount');
+  }
+
+  get caloriesPerUnit() {
+    return this.form.get('caloriesPerUnit');
+  }
+
+  get quantity() {
+    return this.form.get('quantity');
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    console.log(this.form.value);
+  }
 }
--- src/app/products/products-list/products-list.ts
+++ src/app/products/products-list/products-list.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-products-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './products-list.html',
   styleUrl: './products-list.css',
 })
 export class ProductsList {
-
+  readonly products = signal([
+    { id: 0, name: 'test 1' },
+    { id: 1, name: 'test 2' },
+  ]);
 }
