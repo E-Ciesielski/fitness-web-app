259813c4bc4dbefcb6437d6b838abce7944a7373
Eryk C <eryk9903@gmail.com>
2025-12-03 13:45:44 +0100

    add cancel button to product form

--- src/app/products/create-product-page/create-product-page.ts
+++ src/app/products/create-product-page/create-product-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-product-page',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './create-product-page.html',
   styleUrl: './create-product-page.css',
 })
